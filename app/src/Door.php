52a1707a0311a0d6f03ebe3ca54e105ca9296909
Andrés Añez <aeanez@gmail.com>
2021-04-18 15:16:26 -0500

    Adding code to assert the door default state is returned

---
+++ app/src/Door.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Src;
+
+class Door {
+
+    /**
+     * List of posible door states
+     */
+    public array $states = [
+        'closed',
+        'open'
+    ];
+
+    /**
+     * Current door state
+     */
+    public string $currentState = 'closed';
+
+}
