aae0acd01419f6fd2321c0dbaad41589d29ca67f
Andrés Añez <aeanez@gmail.com>
2021-04-18 18:57:26 -0500

    Demo index file updated

--- index.php
+++ index.php
@@ -3,14 +3,18 @@
 
 require_once './vendor/autoload.php';
 
-use Aeanez\Door;
-use Aeanez\Visitor;
+use Aeanez\OneHundredDoors\Door;
+use Aeanez\OneHundredDoors\Visitor;
 
 # Create your how doors list
 $doorsList = [
             new Door(),
             new Door(),
             new Door(),
+            new Door(),
+            new Door(),
+            new Door(),
+            //All the way to 100
         ];
 
 # Create your own visitor
